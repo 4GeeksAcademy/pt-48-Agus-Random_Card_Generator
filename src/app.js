9074554956b488e8b61f3131caf0a0163f2acb1f
Agustín Ruiz <143494692+GoldenDrk@users.noreply.github.com>
2023-10-17 21:31:17 +0000

    Implementación de JS

--- src/app.js
+++ src/app.js
@@ -2,10 +2,17 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let symbols = ["❤", "♦", "♠", "♣"];
+  let numbers = new Array(12).fill(1).map((accumulator, index) => index + 1);
+
+  let randomSymbolIndex = Math.floor(Math.random() * symbols.length);
+  let randomNumberIndex = Math.floor(Math.random() * numbers.length);
+
+  let randomSymbol = symbols[randomSymbolIndex];
+  let randomNumber = numbers[randomNumberIndex];
+
+  document.querySelector("#number").innerHTML = randomNumber;
+  document.querySelector("#symbol").innerHTML = randomSymbol;
+  document.querySelector("#symbol2").innerHTML = randomSymbol;
 };
